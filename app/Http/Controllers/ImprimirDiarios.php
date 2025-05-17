7d2f9abc51cce672604877245316be3ff5cc1c8a
edimilsonqueiroz <edimilsonqueiroz681@gmail.com>
2025-05-17 18:14:44 -0300

    pronto

--- app/Http/Controllers/ImprimirDiarios.php
+++ app/Http/Controllers/ImprimirDiarios.php
@@ -3,26 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class ImprimirDiarios extends Controller
 {
     public function conteudos(Request $request)
     {
-
+        $conteudos = Pdf::loadView('relatorios.conteudo');
+        return $conteudos->stream();
     }
 
     public function frequencia(Request $request)
     {
-
+        $frequencia = Pdf::loadView('relatorios.frequencia');
+        return $frequencia->stream();
     }
 
-    public function boletim(Request $request)
-    {
-
-    }
-
-    public function declaracaoMatricula(Request $request)
-    {
-
-    }
 }
